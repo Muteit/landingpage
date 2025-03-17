05ece0950f5e183aace7837a7b95afcec63fefb1
Olga Nayda <olian2010@ya.ru>
2025-03-17 17:33:00 +0300

    feat: добавлена функция определения мобильных устройств и обновлена логика отображения плеера

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,15 +47,20 @@ const isInViewport = (el) => {
   );
 }
 
+const isMobile = () => {
+  let reg = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i;
+  return reg.test(window.navigator.userAgent);
+}
+
 setTimeout(() => {
   player.classList.remove('d-none');
 }, 300);
 
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', function() {
   if (isInViewport(headerButton)) {
     player.classList.remove('visible');
   } else {
-    if (!player.classList.contains('visible') && tap.classList.contains('play')) {
+    if (!player.classList.contains('visible') && (!isMobile() || tap.classList.contains('play'))) {
       player.classList.add('visible');
     }
   }
